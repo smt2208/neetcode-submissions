062f09f64097b3cbc5c782d7d927228302b84da1
Shyam Mohan Tripathi <tripathishyam22@gmail.com>
2026-07-25 17:48:28 +0530

    Add: first-missing-positive - submission-1

---
+++ Data Structures & Algorithms/first-missing-positive/submission-1.java
@@ -0,0 +1,21 @@
+//good but does not satisfy the no extra space thing
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (num > 0) {
+                set.add(num);
+            }
+        }
+
+        int ans = 1;
+
+        while (set.contains(ans)) {
+            ans++;
+        }
+
+        return ans;
+    }
+}
