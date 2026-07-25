86a8fbcdac26ed218fb76dfc8f51eb2e0c86cec7
Shyam Mohan Tripathi <tripathishyam22@gmail.com>
2026-07-25 16:58:36 +0530

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int key : count.keySet()) {
+            if (count.get(key) > nums.length / 3) {
+                res.add(key);
+            }
+        }
+
+        return res;
+    }
+}
